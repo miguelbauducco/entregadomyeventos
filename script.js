40dd569bef83986c7629ebe95a16466e18a2bb36
miguelbauducco <miguel.bau14@gmail.com>
2022-07-05 13:26:12 -0300

    entrega finalizada

---
+++ script.js
@@ -0,0 +1,36 @@
+const contacto = [];
+
+const formulario = document.getElementById("formulario");
+const nombre= document.getElementById("full-name");
+const correo = document.getElementById("email");
+const consulta = document.getElementsByTagName("textarea");
+
+formulario.addEventListener("submit", (event) => {
+
+
+    event.preventDefault();
+
+    if(nombre.value ===""){
+        console.log("debes completar este campo con tu nombre");
+        return;
+
+    }else if(correo.value === ""){
+        console.log("debes completar este campo con tu email");
+        return;
+
+    }else if(consulta.value === ""){
+        console.log("debes completar este campo con tu consulta");
+        return;
+    }
+
+const contactos = {
+    nombre: nombre.value,
+    email: correo.value,
+    consulta : consulta.value,
+};
+
+contacto.push(contactos);
+
+console.log(contacto);
+
+});
